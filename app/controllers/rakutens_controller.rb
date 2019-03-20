41a79af2b4894b6f9cb2c8672a1e73baf8449e1a
J-Azuma <azumanotdetail+2@gmail.com>
2019-03-21 01:13:41 +0900

    コードを修正

--- app/controllers/rakutens_controller.rb
+++ app/controllers/rakutens_controller.rb
@@ -49,9 +49,9 @@ class RakutensController < ApplicationController
          "contents": 
           { "type": 'carousel',
             "contents":[   
-             for i in 0..2 do
-              make_part(items[i])
-             end
+              make_part(items[0]),
+              make_part(items[1]),
+              make_part(items[2])
           ]}
         }
     end
